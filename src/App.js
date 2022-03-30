b179c7e625a75681260abde2ac2cd9413332e6fe
Ovidio Perez <oapm10@gmail.com>
2022-03-30 14:33:35 -0400

    Change the playPomo function logic (make the timer no depending on the date object)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ class PomodoroClock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            sessionLength: 1,
-            breakLength: 1,
-            totalTime: (1 + 1) * 60000,
+            sessionLength: 25,
+            breakLength: 5,
             isPomoInSession: true,
-            timer: "25:00",
+            timer: 1500,
+            timeLeft: "25:00",
             isTimerPlaying: false,
             pomoIntervalID: 0,
             passedTime: 0,
@@ -36,15 +36,12 @@ class PomodoroClock extends Component {
 
             const incrementedSessionTime = state.sessionLength + 1;
 
-            const newTotalTime =
-                (state.breakLength + incrementedSessionTime) * 60000;
-
             return {
                 sessionLength: incrementedSessionTime,
-                timer:
+                timeLeft:
                     incrementedSessionTime.toString().padStart(2, "0") + ":00",
                 passedTime: 0,
-                totalTime: newTotalTime,
+                timer: incrementedSessionTime * 60,
             };
         });
     }
@@ -54,15 +51,12 @@ class PomodoroClock extends Component {
 
             const decrementedSessionTime = state.sessionLength - 1;
 
-            const newTotalTime =
-                (state.breakLength + decrementedSessionTime) * 60000;
-
             return {
                 sessionLength: decrementedSessionTime,
-                timer:
+                timeLeft:
                     decrementedSessionTime.toString().padStart(2, "0") + ":00",
                 passedTime: 0,
-                totalTime: newTotalTime,
+                timer: decrementedSessionTime * 60,
             };
         });
     }
@@ -73,13 +67,9 @@ class PomodoroClock extends Component {
 
             const incrementedBreakTime = state.breakLength + 1;
 
-            const newTotalTime =
-                (incrementedBreakTime + state.sessionLength) * 60000;
-
             return {
                 breakLength: incrementedBreakTime,
                 passedTime: 0,
-                totalTime: newTotalTime,
             };
         });
     }
@@ -89,13 +79,9 @@ class PomodoroClock extends Component {
 
             const decrementedBreakTime = state.breakLength - 1;
 
-            const newTotalTime =
-                (decrementedBreakTime + state.sessionLength) * 60000;
-
             return {
                 breakLength: decrementedBreakTime,
                 passedTime: 0,
-                totalTime: newTotalTime,
             };
         });
     }
@@ -107,11 +93,11 @@ class PomodoroClock extends Component {
         this.setState({
             sessionLength: 25,
             breakLength: 5,
-            timer: "25:00",
+            timeLeft: "25:00",
+            timer: 1500,
             passedTime: 0,
             isTimerPlaying: false,
             isPomoInSession: true,
-            totalTime: (25 + 5) * 60000,
         });
     }
 
@@ -126,81 +112,46 @@ class PomodoroClock extends Component {
             return;
         }
 
-        const sessionTime =
-            Date.now() +
-            this.state.sessionLength * 60000 -
-            this.state.passedTime;
-        const breakTime =
-            Date.now() +
-            (this.state.breakLength + this.state.sessionLength) * 60000 -
-            this.state.passedTime;
-
         const intervalID = setInterval(() => {
-            const currentTime = Date.now();
-            let timeLeft = 0;
-            let passedTime = 0;
+            this.setState((state, props) => ({
+                timer: state.timer - 1,
+                passedTime: state.passedTime + 1,
+            }));
 
-            if (this.state.isPomoInSession) {
-                timeLeft = sessionTime - currentTime;
-                passedTime = this.state.sessionLength * 60000 - timeLeft;
-            } else {
-                timeLeft = breakTime - currentTime;
-                passedTime =
-                    (this.state.sessionLength + this.state.breakLength) *
-                        60000 -
-                    timeLeft;
-            }
+            let timer = this.state.timer;
+            let timeLeft = timer / 60;
 
-            if (timeLeft <= 0) {
+            if (timer === 0) {
                 this.playAudio();
-            }
+                let newTimer = 0;
+                if (this.state.isPomoInSession) {
+                    newTimer = this.state.breakLength * 60 + 1;
+                } else {
+                    newTimer = this.state.sessionLength * 60 + 1;
+                }
 
-            if (timeLeft <= 0 && this.state.isPomoInSession) {
                 this.setState((state, props) => ({
-                    isPomoInSession: false,
+                    isPomoInSession: !state.isPomoInSession,
+                    timer: newTimer,
+                    passedTime: 0,
                 }));
             }
 
-            if (passedTime >= this.state.totalTime) {
-                clearInterval(this.state.pomoIntervalID);
-                this.setState({
-                    passedTime: 0,
-                    timer: `${"0".padStart(2, "0")}:${"0".padStart(2, "0")}`,
-                    isTimerPlaying: false,
-                    isPomoInSession: true,
-                });
-                this.playPomo();
-                return;
+            let [minutesLeft, secondsLeft] = timeLeft.toString().split(".");
+
+            if (!secondsLeft) {
+                secondsLeft = "0";
+            } else {
+                secondsLeft = (("0." + secondsLeft) * 60).toFixed(0);
             }
-
-            /**
-             * Stores the time that has passed since the pomo started
-             */
-            this.setState({
-                passedTime: passedTime,
-            });
-
-            /**
-             * Stores the minutes and seconds remaining until the pomo is finished.
-             */
-            let [minutesLeft, secondsLeft] = (timeLeft / 60000)
-                .toString()
-                .split(".");
-
-            secondsLeft = (("0." + secondsLeft) * 60).toFixed(0);
-
-            // if (secondsLeft === "60") {
-            //     minutesLeft = (parseInt(minutesLeft) + 1).toString();
-            //     secondsLeft = "0";
-            // }
 
             if (minutesLeft === "-0") minutesLeft = "0";
 
             this.setState({
-                timer: `${minutesLeft.padStart(2, "0")}:${secondsLeft.padStart(
+                timeLeft: `${minutesLeft.padStart(
                     2,
                     "0"
-                )}`,
+                )}:${secondsLeft.padStart(2, "0")}`,
                 isTimerPlaying: true,
             });
         }, 1000);
@@ -211,8 +162,8 @@ class PomodoroClock extends Component {
     render() {
         const sessionLength = this.state.sessionLength;
         const breakLength = this.state.breakLength;
-        const pomoStage = this.state.isPomoInSession;
-        const timeLeft = this.state.timer;
+        const pomoStatus = this.state.isPomoInSession;
+        const timeLeft = this.state.timeLeft;
         return (
             <div className="App">
                 <h1>Pomodoro Clock</h1>
@@ -224,7 +175,7 @@ class PomodoroClock extends Component {
                     incrementBreak={this.incrementBreakLength}
                     decrementBreak={this.decrementBreakLength}
                 />
-                <Timer status={pomoStage} time={timeLeft} />
+                <Timer status={pomoStatus} time={timeLeft} />
                 <PomoControllers
                     play={this.playPomo}
                     reset={this.resetPomoValues}
